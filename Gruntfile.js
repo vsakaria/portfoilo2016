68c4ecb7754782a63c9e397b5eb5e9ef8282c418
Vishal Sakaria <vsakaria@gmail.com>
2015-09-28 17:36:36 +0100

    Init commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,48 +10,32 @@ module.exports = function(grunt) {
             build: {
                 options: {
                     sassDir: 'assets/sass',
-                    cssDir: 'assets/css',
-                    outputStyle: 'compressed'
+                    cssDir: 'assets/css'
                 }
             }
         },
 
         // Clean
         clean: {
-            pre: ['styleguide', 'assets/css'],
+            pre: ['assets/css'],
             post: ['.sass-cache']
         },
 
         // Watch
         watch: {
             sass: {
-                files: ['assets/sass/**/*.{sass,scss}'],
-                tasks: ['compass', 'sassdown']
-            }
-        },
-
-        // Sassdown (Styleguide)
-        sassdown: {
-            options: {
-                assets: ['assets/css/*.css']
-            },
-            files: {
-                expand: true,
-                cwd: 'assets/sass/partials',
-                src: ['**/*.{sass,scss}'],
-                dest: 'styleguide/'
+                files: ['assets/sass/*.{sass,scss}'],
+                tasks: ['compass']
             }
         }
-
     });
 
     // Load NPM Tasks
-    grunt.loadNpmTasks('sassdown');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compass');
 
     // Register Grunt tasks
-    grunt.registerTask('default', ['clean:pre', 'compass', 'sassdown', 'clean:post']);
-  
+    grunt.registerTask('default', ['clean:pre', 'compass', 'clean:post']);
+
 };
