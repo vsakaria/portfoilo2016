42ccd803468fadbd5e975852705de9c0f73cb92f
Vishal Sakaria <vsakaria@gmail.com>
2015-10-13 22:00:30 +0100

    Removed all unneeded files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,3 @@
 $(function() {
-    $('#slides').slidesjs({
-        width: 940,
-        height: 440,
-        pagination: {
-            active: false
-        },
-        navigation: {
-            active: true
-        }
-    });
-
-    $('.slidesjs-previous').empty().append("<img src='./images/right-arrow.png'>");
-
-    $('.slidesjs-next').empty().append("<img src='./images/left-arrow.png'>");
-
-    $('.burger-container').on('click', function() {
-        $('.page-nav').toggle()
-    });
-
-    setTimeout(function() {
-        $('.slidesjs-next').trigger('click');
-    }, 1500);
 
 });
