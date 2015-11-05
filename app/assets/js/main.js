7d345133b521a9fb6d930c6fe4dfe13d165413c2
Vishal Sakaria <vsakaria@gmail.com>
2015-11-05 18:59:56 +0000

    Added links to menu

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,7 +1,21 @@
-$(function() {
+$( document ).ready(function() {
     $('.burger-container').on('click', function () {
         $('.nav-location header').toggleClass('animate-location-title');
         $('.nav-container').toggleClass('animate-nav');
         $('.burger-line').toggleClass('open');
+    });
+
+    $('.nav-container').on('click', function (e) {
+        var title = e.target.innerHTML;
+        var page = $(e.target).data('page');
+
+        $('.nav-title').html(title);
+        $('.nav-location header').toggleClass('animate-location-title');
+        $(this).toggleClass('animate-nav');
+
+        $('.main-container').hide();
+        $('.' + page).toggle();
+
+        return false;
     });
 });
