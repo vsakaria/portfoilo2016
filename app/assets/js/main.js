912828689cd99f21ff6e99efb3b33ab52ca10b24
Vishal Sakaria <vsakaria@gmail.com>
2015-11-07 21:53:17 +0000

    Add content for 768 breakpoint

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,11 +1,11 @@
-$( document ).ready(function() {
-    $('.burger-container').on('click', function () {
+$(document).ready(function() {
+    $('.burger-container').on('click', function() {
         $('.nav-location header').toggleClass('animate-location-title');
         $('.nav-container').toggleClass('animate-nav');
         $('.burger-line').toggleClass('open');
     });
 
-    $('.nav-container').on('click', function (e) {
+    $('.nav-container').on('click', function(e) {
         var title = e.target.innerHTML;
         var page = $(e.target).data('page');
 
@@ -18,4 +18,13 @@ $( document ).ready(function() {
 
         return false;
     });
+
+    $(window).resize(function() {
+        var width = $(window).width();
+        if (width >= 768) {
+            $('.nav-container').removeClass('nav-container-mobile').addClass('nav-container-large');
+        } else {
+            $('.nav-container').removeClass('nav-container-large').addClass('nav-container-mobile');
+        }
+    }).resize();
 });
