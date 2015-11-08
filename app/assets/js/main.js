ced4244db12adb45a0872441836acd1e638b57ee
Vishal Sakaria <vsakaria@gmail.com>
2015-11-08 18:47:15 +0000

    Added content and nav for tablet

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,23 +1,4 @@
 $(document).ready(function() {
-    $('.burger-container').on('click', function() {
-        $('.nav-location header').toggleClass('animate-location-title');
-        $('.nav-container').toggleClass('animate-nav');
-        $('.burger-line').toggleClass('open');
-    });
-
-    $('.nav-container').on('click', function(e) {
-        var title = e.target.innerHTML;
-        var page = $(e.target).data('page');
-
-        $('.nav-title').html(title);
-        $('.nav-location header').toggleClass('animate-location-title');
-        $(this).toggleClass('animate-nav');
-
-        $('.main-container').hide();
-        $('.' + page).toggle();
-
-        return false;
-    });
 
     $(window).resize(function() {
         var width = $(window).width();
@@ -27,4 +8,41 @@ $(document).ready(function() {
             $('.nav-container').removeClass('nav-container-large').addClass('nav-container-mobile');
         }
     }).resize();
+
+    $('.burger-container').on('click', function() {
+        $('.nav-location header').toggleClass('animate-location-title');
+        $('.nav-container').toggleClass('animate-nav');
+        $('.burger-line').toggleClass('open');
+    });
+
+    $('.nav-container-mobile').on('click', function(e) {
+        var title = e.target.innerHTML;
+        var page = $(e.target).data('page');
+
+        $('.nav-title').html(title);
+        $('.nav-location header').toggleClass('animate-location-title');
+        $('.nav-container').toggleClass('animate-nav');
+
+        $('.main-container').hide();
+        $('.' + page).toggle();
+
+        return false;
+    });
+
+
+    $('.nav-container-large ul li a').on('click', function(e) {
+        var title = e.target.innerHTML;
+        var page = $(e.target).data('page');
+
+        $('.nav-title').html(title);
+        $('.nav-location header').toggleClass('animate-location-title');
+
+        $('.main-container').hide();
+        $('.' + page).toggle();
+
+        $('.nav-location header').toggleClass('animate-location-title');
+
+
+        return false;
+    });
 });
